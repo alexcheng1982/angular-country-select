a8344522e19ced888515ef18c9d43a61cd41e2ff
Fu Cheng <alexcheng1982@gmail.com>
2015-01-15 21:21:53 +1300

    Use select2 and build countries json file

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Country select for AngularJS
 ======================
 
-A simple AngularJS directive to create country select. It uses Twitter [typeahead.js](http://twitter.github.io/typeahead.js/) to create auto-complete country select. Country data is from [mledoze/countries](https://github.com/mledoze/countries).
+A simple AngularJS directive to create country select. It uses Twitter [select2](http://select2.github.io/select2/) to create auto-complete country select. Country data is from [mledoze/countries](https://github.com/mledoze/countries).
 
 ### Usage
 
@@ -14,7 +14,7 @@ angular.module('countrySelectExample', ['angular-country-select']);
 Then use directive `country-select`.
 
 ```html
-<input country-select type="text">
+<input country-select>
 ```
 
 See `example.html` for a simple example.
--- angular-country-select.js
+++ angular-country-select.js
@@ -2,29 +2,11 @@ angular.module('angular-country-select', [])
   .directive('countrySelect', [function() {
     return {
       restrict: 'A',
-      link: function(scope, elem, attrs) {
-        var countries = new Bloodhound({
-          datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
-          queryTokenizer: Bloodhound.tokenizers.whitespace,
-          limit: 10,
-          remote: {
-            url: '../data/countries.json',
-            filter: function(countries) {
-              return $.map(countries, function(country) {
-                return {
-                  name: country.name.common
-                };
-              });
-            }
-          }
-        });
-
-        countries.initialize();
-        
-        elem.typeahead(null, {
-          name: 'countries',
-          displayKey: 'name',
-          source: countries.ttAdapter()
+      require:'ngModel',
+      link: function(scope, elem, attrs, ngModelCtrl) {
+        var data = [];
+        elem.select2({
+          data: data
         });
       }
     };
